cf1894c8e88340ee95412422b75dacdb5ec9437d
NiloLP <112017951+NiloLP@users.noreply.github.com>
2023-08-20 06:25:41 +0000

    commit "falta"

---
+++ src/main/js/app.js
@@ -0,0 +1,24 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { createBrowserRouter, RouterProvider } = require('react-router-dom');
+
+const PageHome = require('./pages/home');
+const PageNuevoMusico = require('./pages/nuevo-musico');
+const PageEditarMusico = require('./pages/editar-musico');
+const PageEditarInstrumento = require('./pages/editar-instrumento');
+
+
+const router = createBrowserRouter([
+	{ path: '/', element: <PageHome /> },
+	{ path: '/nuevo-musico', element: <PageNuevoMusico /> },
+	{ path: '/editar-musico/:id', element: <PageEditarMusico /> },
+	{ path: '/editar-instrumento/:id', element: <PageEditarInstrumento /> }
+]);
+
+
+ReactDOM.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+	document.getElementById('react')
+)
